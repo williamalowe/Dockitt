24391495e04a504025869a5f3c1593679e4184bd
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-03-27 19:03:37 +1100

    stlyes: add task-view styling

--- src/views/Tasks/Tasks.jsx
+++ src/views/Tasks/Tasks.jsx
@@ -1,9 +1,24 @@
+import { useContext, useState } from 'react';
+import { TaskContext } from '../Home/Home';
 import styles from './Tasks.module.css';
 
 const Tasks = () => {
+  const { tasks, addTask } = useContext(TaskContext);
+  const [newDesc, setNewDesc] = useState(null);
+  const [newPrio, setNewPrio] = useState(null);
+  const [newStat, setNewStat] = useState(null);
+  
+  const addNewTask = () => {
+    addTask(newDesc, newPrio, newStat);
+  };
+  
+  console.log(tasks);
   return (
     <section className={styles.main}>
-      Tasks
+      <div className={styles.header}>
+        <h3>Welcome back.</h3>
+        <h5>Here are your current tasks.</h5>
+      </div>
     </section>
   )
 }
