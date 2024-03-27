0343d99c54a0c0b6776b1a729f2156a49afae47c
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-03-27 18:12:11 +1100

    feat: add logoheader-component

---
+++ src/components/LogoHeader/LogoHeader.jsx
@@ -0,0 +1,13 @@
+import styles from './LogoHeader.module.css';
+
+const LogoHeader = () => {
+  return (
+    <div className={styles.header}>
+      <h3>
+        dockitt.
+      </h3>
+    </div>
+  )
+}
+
+export default LogoHeader
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,10 @@
+import LogoHeader from '../../components/LogoHeader/LogoHeader';
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
     <main className={styles.main}>
-      Hello World
+      <LogoHeader />
     </main>
   )
 }
