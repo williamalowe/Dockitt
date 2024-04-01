2a306e42ec13e292f28ca07973e85c7a8bbe36d8
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-01 16:21:51 +1100

    feat: add default-view

--- src/utils/Router/Router.jsx
+++ src/utils/Router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../../views/Home/Home";
 import Tasks from "../../views/Tasks/Tasks";
 import Error from "../../views/Error/Error";
+import Default from "../../views/Default/Default";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -9,6 +10,7 @@ const Router = () => {
       path: '/',
       element: <Home />,
       children: [
+        { index: true, element: <Default />},
         { path: '/tasks', element: <Tasks />}
       ],
       errorElement: <Error />
---
+++ src/views/Default/Default.jsx
@@ -0,0 +1,12 @@
+import styles from './Default.module.css';
+
+const Default = () => {
+  return (
+    <section className={styles.main}>
+      <h3>Welcome to <span className={styles.title}>dockitt</span>.</h3>
+      <p></p>
+    </section>
+  )
+}
+
+export default Default
