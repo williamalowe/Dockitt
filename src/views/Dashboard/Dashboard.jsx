6c3d5f2113701a22b15245e14fe537e5a5a11908
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-02 12:55:08 +1100

    feat: add StatCard-components

---
+++ src/views/Dashboard/Dashboard.jsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import { TaskContext } from '../Home/Home';
+import styles from './Dashboard.module.css';
+import StatCard from '../../components/StatCard/StatCard';
+
+const Dashboard = () => {
+  const { tasks } = useContext(TaskContext);
+  const activeTasks = tasks.filter((task) => task.status !== 'cancelled')
+
+  return (
+    <section className={styles.dashboard}>
+      <div className={styles.upper}>
+        <h3>Dashboard</h3>
+        <div className={styles.upperContent}>
+          <div className={styles.numbers}>
+            <StatCard 
+              title="Backlog"
+              number={activeTasks.filter((task) => task.status === 'backlog').length}
+              total={activeTasks.length}
+            />
+          </div>
+          <div className={styles.numbers}>
+            <StatCard 
+              title="In Progress"
+              number={activeTasks.filter((task) => task.status === 'in progress').length}
+              total={activeTasks.length}
+            /></div>
+          <div className={styles.numbers}>
+            <StatCard 
+              title="Under Review"
+              number={activeTasks.filter((task) => task.status === 'under review').length}
+              total={activeTasks.length}
+            /></div>
+          <div className={styles.numbers}>
+            <StatCard 
+              title="Completed"
+              number={activeTasks.filter((task) => task.status === 'completed').length}
+              total={activeTasks.length}
+            /></div>
+        </div>
+      </div>
+      <div className={styles.lower}>
+        <div className={styles.lowerLeft}></div>
+        <div className={styles.lowerRight}></div>
+      </div>
+    </section>
+  )
+}
+
+export default Dashboard
